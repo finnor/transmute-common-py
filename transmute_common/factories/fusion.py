22b268cb81bd7f10043a90d2a12b75090752c566
Adrian Flannery <aflanry@gmail.com>
2022-06-09 15:35:10 -0500

    Updated fusions

--- transmute_common/factories/fusion.py
+++ transmute_common/factories/fusion.py
@@ -1,11 +1,15 @@
 def create(values = None):
   record = {
     "name": None,
+    "genomeBuild": None,
+    "coordinateSystem": None,
     "5pGene": None,
+    "5pTranscript": None,
     "5pExon": None,
     "5pChrom": None,
     "5pPos": None,
     "3pGene": None,
+    "3pTranscript": None,
     "3pExon": None,
     "3pChrom": None,
     "3pPos": None,
@@ -14,7 +18,6 @@ def create(values = None):
     "read_count": None,
     "normalizedExpression": None,
     "normalization": None,
-    "frame": None,
     "interpretation": None,
     "therapyRecommendation": None,
   }
